94dffbe086ecb80c30ece96117c7b478d079af6f
Ramses Ramos <ramses1030@live.com.mx>
2023-01-16 15:18:37 -0600

    cambios

--- mcsiotrb/js/scripts.js
+++ mcsiotrb/js/scripts.js
@@ -12,6 +12,25 @@ function respuesta(num_pregunta, seleccionada){
     opcion_elegido[num_pregunta] = seleccionada.value;
 
     //el siguiente codigo es para poner
-    //el fondo de los inputs
+    //el fondo de los inputs para cuanod eliga otra opcion
     //armo el id para seleccionar el section correspondiente
+    id="p" + num_pregunta;
+
+    labels = document.getElementById(id).childNodes;
+    labels[3].style.backgroundColor = "white";
+    labels[5].style.backgroundColor = "white";
+    labels[7].style.backgroundColor = "white";
+
+    //doy el color a label seleccionado
+    seleccionada.parentNode.style.backgroundColor = "cec0fc";
+}
+
+//funcion que compara los arreglos para saber cuantas estuvieron correctas
+function corregir(){
+    cantidad_correctas = 0;
+    for(i=0; i < correctas.length;i++){
+        if(correctas[i]==opcion_elegido[i]){
+            cantidad_correctas++;
+        }
+    }
 }
